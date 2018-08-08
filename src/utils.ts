f4ab6b7b66b3bbed3b16b0afbdcd910d8735a630
Yulong Ruan <yruan@alpha-sense.com>
2018-08-08 16:58:45 +0300

    remove useless code

--- src/utils.ts
+++ src/utils.ts
@@ -1,8 +1,5 @@
-import { flip, uncurryN, compose, curry } from 'ramda'
-
 export const setField = (field: string) => (value: any) => (state: any) => state.set(field, value)
 export const getField = (field: string) => (state: any) => state.get(field)
 export const incField = (field: string) => (state: any) => state.set(field, state.get(field) + 1)
 export const decField = (field: string) => (state: any) => state.set(field, state.get(field) - 1)
 export const toggleField = (field: string) => (state: any) => state.set(field, !state.get(field))
-export const flipCurry = compose(curry, flip, uncurryN(2))
