0487807d089f0894d1d307bda36293a7122ab745
ruanyl <ruanyu1@gmail.com>
2019-02-11 20:38:46 +0200

    remove utils & export interface StateObject

--- src/createState.ts
+++ src/createState.ts
@@ -1,7 +1,5 @@
-import { Record } from 'immutable'
+import { Record as ImmutableRecord } from 'immutable'
 import { compose } from 'ramda'
-
-import { getField, setField } from './utils'
 
 export interface StateConfig<T> {
   namespace: string;
@@ -13,9 +11,22 @@ export interface GlobalState {
   __root: any;
 }
 
-export type LocalState<T> = Record<T> & Readonly<T>
+export type LocalState<T> = ImmutableRecord<T> & Readonly<T>
 
-export const createState = <T extends object, K extends keyof T>(config: StateConfig<T>) => {
+export const setField = <T extends Record<string, any>, K extends keyof T>(field: K, value: T[K]) =>
+  (state: LocalState<T>) => state.set(field, value)
+
+export const getField = <T extends Record<string, any>, K extends keyof T>(field: K) =>
+  (state: LocalState<T>) => state.get(field)
+
+export interface StateObject<T extends Record<string, any>, K extends keyof T> {
+  create: () => LocalState<T>
+  get: (k: K) => (s: LocalState<T>) => LocalState<T>
+  set: (k: K, v: T[K]) => (s: LocalState<T>) => LocalState<T>
+  namespace: string
+}
+
+export const createState = <T extends Record<string, any>, K extends keyof T>(config: StateConfig<T>): StateObject<T, K> => {
   const g = (state: GlobalState | LocalState<T>) => {
     if (state.has('__root')) {
       return state.get(config.namespace)
@@ -24,7 +35,7 @@ export const createState = <T extends object, K extends keyof T>(config: StateCo
   }
 
   const create = () => {
-    const StateShape = Record<T>(config.fields)
+    const StateShape = ImmutableRecord<T>(config.fields)
     return new StateShape()
   }
 
--- src/utils.ts
+++
@@ -1,7 +0,0 @@
-import { Record } from 'immutable'
-
-export const setField = <T extends object, K extends keyof T>(field: K, value: T[K]) => (state: Record<T> & Readonly<T>) => state.set(field, value)
-export const getField = <T extends object, K extends keyof T>(field: K) => (state: Record<T> & Readonly<T>) => state.get(field)
-export const incField = (field: string) => (state: any) => state.set(field, state.get(field) + 1)
-export const decField = (field: string) => (state: any) => state.set(field, state.get(field) - 1)
-export const toggleField = (field: string) => (state: any) => state.set(field, !state.get(field))
