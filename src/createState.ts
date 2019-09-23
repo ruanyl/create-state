d1de088ae02e31356b6cb31dbf0731f725bfc2f6
Yulong Ruan <ruanyu1@gmail.com>
2019-09-23 21:33:31 +0300

    rename: LocalState -> State

--- src/createState.ts
+++ src/createState.ts
@@ -7,17 +7,17 @@ export interface StateConfig<T> {
 
 export type GlobalState = Map<string, any>
 
-export type LocalState<T> = ImmutableRecord<T> & Readonly<T>
+export type State<T> = ImmutableRecord<T> & Readonly<T>
 
 export type Selectors<T extends Record<string, any>> = {
   [K in keyof T]: (state: GlobalState) => T[K]
 }
 
 export interface StateObject<T extends Record<string, any>> {
-  create: () => LocalState<T>
+  create: () => State<T>
   get: <K extends keyof T>(k: K) => (s: GlobalState) => T[K]
-  set: <K extends keyof T>(k: K, v: T[K]) => (s: LocalState<T>) => LocalState<T>
-  toggle: <K extends keyof T>(k: K) => (s: LocalState<T>) => LocalState<T>
+  set: <K extends keyof T>(k: K, v: T[K]) => (s: State<T>) => State<T>
+  toggle: <K extends keyof T>(k: K) => (s: State<T>) => State<T>
   selectors: Selectors<T>
   namespace: string
 }
@@ -30,15 +30,15 @@ export const createState = <T extends Record<string, any>>(namespace: string, fi
   }
 
   const get = <K extends keyof T>(k: K) => (state: GlobalState) => {
-    const localState: LocalState<T>  = state.get(namespace)
+    const localState: State<T>  = state.get(namespace)
     return localState.get(k)
   }
 
-  const set = <K extends keyof T>(k: K, v: T[K]) => (state: LocalState<T>) => {
+  const set = <K extends keyof T>(k: K, v: T[K]) => (state: State<T>) => {
     return state.set(k, v)
   }
 
-  const toggle = <K extends keyof T>(k: K) => (state: LocalState<T>) => {
+  const toggle = <K extends keyof T>(k: K) => (state: State<T>) => {
     const current = state.get(k)
     if (typeof current === 'boolean') {
       return state.set(k, !current as any)
